91211afa1caa02aa6c8700aa0a3eeaca9f0f21ac
jean-nathan <nathan.ecom@outlook.com>
2024-04-25 15:46:17 -0300

    ajustado comandos para instalacao o python3-env

--- executar_myworkbench.sh
+++ executar_myworkbench.sh
@@ -7,16 +7,42 @@ if [ -z "$VIRTUAL_ENV" ]; then
     source myenv/bin/activate
 fi
 
+# Função para instalar o pacote python3-venv no Ubuntu/Debian
+install_python3_venv_ubuntu() {
+    echo "Instalando python3-venv no Ubuntu/Debian..."
+    sudo apt update
+    sudo apt install -y python3-venv
+}
+
+# Função para instalar o pacote python3-venv no macOS usando Homebrew
+install_python3_venv_macos() {
+    echo "Instalando python3-venv no macOS..."
+    brew update
+    brew install python3
+}
+
+# Verificar e instalar o pacote python3-venv se não estiver disponível
+if ! [ -x "$(command -v python3 -m venv)" ]; then
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        install_python3_venv_ubuntu
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        install_python3_venv_macos
+    else
+        echo "Sistema operacional não suportado."
+        exit 1
+    fi
+fi
+
 # Verificar e instalar dependências necessárias
 if ! command -v python3 &> /dev/null; then
     echo "Python3 não encontrado. Instalando Python..."
-    sudo apt-get update
-    sudo apt-get install -y python3
+    sudo apt update
+    sudo apt install -y python3
 fi
 
 if ! command -v pip &> /dev/null; then
     echo "pip não encontrado. Instalando pip..."
-    sudo apt-get install -y python3-pip
+    sudo apt install -y python3-pip
 fi
 
 echo "Instalando dependências..."

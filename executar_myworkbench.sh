b33101abc174692462f045b45efde667a0dab991
jean-nathan <nathan.ecom@outlook.com>
2024-04-25 16:03:46 -0300

    ajustado comandos para instalacao automatica

--- executar_myworkbench.sh
+++ executar_myworkbench.sh
@@ -3,49 +3,34 @@
 # Verificar se o ambiente virtual está ativado
 if [ -z "$VIRTUAL_ENV" ]; then
     echo "Criando e ativando ambiente virtual..."
+    
+    # Verificar e instalar python3-venv se necessário
+    if ! command -v python3-venv &> /dev/null; then
+        echo "Instalando python3-venv..."
+        sudo apt-get update
+        sudo apt-get install -y python3-venv
+    fi
+    
+    # Criar e ativar o ambiente virtual
     python3 -m venv myenv
     source myenv/bin/activate
-fi
-
-# Função para instalar o pacote python3-venv no Ubuntu/Debian
-install_python3_venv_ubuntu() {
-    echo "Instalando python3-venv no Ubuntu/Debian..."
-    sudo apt update
-    sudo apt install -y python3-venv
-}
-
-# Função para instalar o pacote python3-venv no macOS usando Homebrew
-install_python3_venv_macos() {
-    echo "Instalando python3-venv no macOS..."
-    brew update
-    brew install python3
-}
-
-# Verificar e instalar o pacote python3-venv se não estiver disponível
-if ! [ -x "$(command -v python3 -m venv)" ]; then
-    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        install_python3_venv_ubuntu
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-        install_python3_venv_macos
-    else
-        echo "Sistema operacional não suportado."
-        exit 1
-    fi
 fi
 
 # Verificar e instalar dependências necessárias
 if ! command -v python3 &> /dev/null; then
     echo "Python3 não encontrado. Instalando Python..."
-    sudo apt update
-    sudo apt install -y python3
+    sudo apt-get update
+    sudo apt-get install -y python3
 fi
 
 if ! command -v pip &> /dev/null; then
     echo "pip não encontrado. Instalando pip..."
-    sudo apt install -y python3-pip
+    sudo apt-get install -y python3-pip
 fi
 
+# Instalar as dependências necessárias no ambiente virtual
 echo "Instalando dependências..."
+source myenv/bin/activate  # Ativar o ambiente virtual antes de instalar as dependências
 pip install mysql-connector-python pandas
 
 # Executar o script Python
